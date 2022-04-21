03296bd19cb9100ff57d41b1239541dfd0f987dd
Ashwin Unnikrishnan <uashwin@student.nitw.ac.in>
2022-04-20 22:06:40 -0400

    Add files via upload
    Adding the Calibration codes
    
    Data Folder : Contains the images used for calibration
    calib Folder : Contains the pkl of the dist and camera matrix
    lib Folder : Contains the library files
    calibration.py : code to run

---
+++ lib/loggingCalib.py
@@ -0,0 +1,15 @@
+# Created loggingCalib.py by histravelstories on 8:36 PM under Project6
+
+
+import logging
+def get_logger(name):
+    log_format = '%(asctime)s  %(name)8s  %(levelname)5s  %(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        format=log_format,
+                        filename='../logging/calibration.log',
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(logging.Formatter(log_format))
+    logging.getLogger(name).addHandler(console)
+    return logging.getLogger(name)
